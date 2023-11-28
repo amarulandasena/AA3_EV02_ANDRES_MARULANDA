d5ba1889b63a5e26b8ff15e7096e0f303873f48d
Andres_Marulanda <afmsamsung1981@gmail.com>
2023-11-28 11:46:55 -0500

    Se resuelve el ejercicio número 3 mostrando en un alert los 2 vectores ingresados por el usuario y la concatenación de ambos ordenados de manera ascendente.

---
+++ src/solucionEjercicio3.js
@@ -0,0 +1,57 @@
+
+const Ejercicio3 = () => {
+
+    /** Declarar los vectores para el ingreso y la salida de los datos */
+    var vector1 = [ ];
+    var vector2 = [ ];
+    var combinacionVectores = [ ];
+
+    /** Función para el ingreso de los datos en los vectores. */
+    const cargaVectores = () => {
+        vector3 = [ ];                                          /** Vector que será retornado con los datos cargados. */
+        let contador = 1;                                       /** Variable contadora para control del ingreso de los datos. */
+
+        let numero = parseInt(prompt("Ingrese el primer número: "));
+        vector3.push(numero);                                   /** Ingresamos el primer dato del vector. */
+
+        let posicion = 0;                                       /** Indicar la posición del vector donde se encuentra el dato */
+        while (contador < 5) {                                  /** Terminamos de cargar el vector. */
+            numero  = parseInt(prompt("Ingrese un número mayor que el anterior: ")) ;
+            vector3.push(numero);
+
+            if (vector3[posicion] > vector3[contador]) {
+                alert("Error, ingrese un número mayor.");
+                vector3.pop();                                  /** Eliminamos el último dato ingresado. */
+                /**continue; */      
+            }else{
+                contador++;                                     /** Si el dato es valido, incrementamos el contador de datos. */
+                posicion++;
+            };
+            
+        };
+        return vector3;
+    };
+
+    vector1 = cargaVectores();
+    vector2 = cargaVectores();
+
+    /** Llamar la función para combinar ambos vectores. */
+    combinacionVectores = vector1.concat(vector2);
+
+    /** Ordenar el vector de manera ascendente */
+    let cambio = 0;
+    for (let i = 0; i < 10; i++){
+        for (let j = i+1; j <= 10; j++){
+            if (combinacionVectores[i] > combinacionVectores[j]){
+                cambio = combinacionVectores[i];
+                combinacionVectores[i] = combinacionVectores[j];
+                combinacionVectores[j] = cambio;
+            };
+        };
+    };
+
+    alert("[" + vector1 + "]" + "\n" +
+          "[" + vector2 + "]" + "\n" +
+          "[" + combinacionVectores + "]");
+    
+};
